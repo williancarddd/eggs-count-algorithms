87f0463f7824209087830378947fb7d0e171b8d3
william cardoso <williancard123@gmail.com>
2025-01-24 14:14:45 -0400

    refactor

--- Labels Eggs.v9i.yolov11/test.py
+++ Labels Eggs.v9i.yolov11/test.py
@@ -1,23 +1,30 @@
 import os
+import torch
 from ultralytics import YOLO
 
-model = YOLO("yolo11n.yaml", ch=1).load(
-    "/media/williancaddd/CODES/fiotec/AETrampa/Labels Eggs.v9i.yolov11/runs/detect/train3/weights/best.pt")
+# Set CUDA environment variables
+os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
+
+# Clear CUDA cache
+torch.cuda.empty_cache()
+
+# Load and train model
+model = YOLO("yolo11n.yaml").load("/media/williancaddd/CODES/fiotec/eggs-count-algorithms/Labels Eggs.v9i.yolov11/runs/detect/train3/weights/best.pt")
 results = model.train(
-    data='/media/williancaddd/CODES/fiotec/AETrampa/Labels Eggs.v9i.yolov11/data.yaml',
-    epochs=200,          # Número de épocas (mantido)
-    imgsz=512,           # Resolução das imagens (mantido)
-    batch=-1,            # Batch size ajustado para um valor razoável
-    lr0=0.001,           # Taxa de aprendizado inicial
-    momentum=0.937,      # Valor típico para YOLO
-    weight_decay=0.001,  # Regularização para evitar overfitting
-    save=True,           # Salvar os checkpoints
-    resume=True,         # Retomar o treinamento
-    workers=4,           # Número de threads de leitura de dados
-    optimizer='AdamW',   # Melhor otimização para problemas complexos
-    iou=0.3,            # Limiar de IoU para detecção
-    conf=0.2,          # Limiar de confiança para detecção
-    nms=True,            # Limiar de NMS para detecção,
-    augment=True,        # Aumento de dados,
-    
+    data='/media/williancaddd/CODES/fiotec/eggs-count-algorithms/Labels Eggs.v9i.yolov11/data.yaml',
+    epochs=200,
+    imgsz=512,          # Reduced image size
+    batch=-1,            # Smaller batch size
+    lr0=0.001,
+    momentum=0.937,
+    weight_decay=0.001,
+    save=True,
+    resume=True,
+    workers=1,          # Reduced workers
+    optimizer='AdamW',
+    iou=0.3,
+    conf=0.2,
+    nms=True,
+    augment=True,
+    amp=True,           # Mixed precision training
 )
